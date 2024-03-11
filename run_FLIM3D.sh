16221585557845dd31bdf1ba978dd808b2279730
Binglin Shen <59431309+shenblin@users.noreply.github.com>
2024-03-11 10:34:06 +0800

    Update run_FLIM3D.sh

--- run_FLIM3D.sh
+++ run_FLIM3D.sh
@@ -15,19 +15,9 @@
 #python basicsr/test.py -opt options/FLIM3D/FLIM3D_PECBNet_temporal_sparsity_test.yml
 
 
-
 python basicsr/test.py -opt options/FLIM3D/FLIM3D_PECBNet_endoscopy_test.yml
-
 #python basicsr/test.py -opt options/FLIM3D_PECBNet_16_channels_test.yml
 
-##################  stack dataset  #######################
+##################  PECBNet  #######################
 # python basicsr/train.py -opt options/FLIM3D/FLIM3D_PECBNet_stack3D_train.yml
 # python basicsr/test.py -opt options/FLIM3D/FLIM3D_PECBNet_stack3D_test.yml
-
-# python basicsr/train.py -opt options/FLIM3D/FLIM3D_PECBNet_stack3D_sparse_frames_train.yml
-
-
-
-##################  SRS dataset  #######################
-# python basicsr/train.py -opt options/SRS/SRS_PECBNet_train.yml
-#python basicsr/test.py -opt options/SRS/SRS_PECBNet_test.yml
